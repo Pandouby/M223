a4eaf19cb29ec3a75b9ff7710f4f7386c0aa9c32
Pandouby <57749377+Pandouby@users.noreply.github.com>
2022-09-19 09:28:55 +0200

    initial commit with base porject

---
+++ m223/src/main/java/ch/sid/M223Application.java
@@ -0,0 +1,13 @@
+package ch.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M223Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M223Application.class, args);
+    }
+
+}
